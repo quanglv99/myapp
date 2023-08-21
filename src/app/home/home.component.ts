c5e3204d209278443fa608876cdbfd86a468233a
quanglv99 <vanquang.vnl21@gmail.com>
2023-08-21 11:34:36 +0700

    fix_guard_20230821

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,49 +3,78 @@ import { Router } from '@angular/router';
 import { AppConfigService } from '../shared/services/app.service';
 import { HttpClient } from '@angular/common/http';
 import { TableUser } from '../shared/models/tableUser.interface';
-import { AuthGuard } from '../auth.guard';
 import { AuthService } from '../shared/services/auth.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation-dialog.component';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  data:TableUser[]=[];
+  data: TableUser[] = [];
   currentPage = 1; // Current page number
   totalItems = 0;
-  constructor(private router:Router,private appConfig :AppConfigService,
-              private http:HttpClient,private authService: AuthService,
-              private dialog: MatDialog) { }
+  searchText: string = '';
+  filterData: TableUser[] = [];
+  isSearchActive: boolean = false;
+  constructor(
+    private router: Router,
+    private appConfig: AppConfigService,
+    private http: HttpClient,
+    private authService: AuthService,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     const url = this.appConfig.getHomeUrl();
-    this.http.get(url)
-    .subscribe((arrData:any)=>{
-      this.data =arrData;
-    },
-    (error)=>{
-      console.log(error)
-    })
-    
+    this.http.get(url).subscribe(
+      (arrData: any) => {
+        this.data = arrData;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
-  onSignout(){
+  onSignout() {
     const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
       width: '300px',
-      data: { message: 'Are you sure you want to sign out?',
-              showYesNo: true }
+      data: { message: 'Are you sure you want to sign out?', showYesNo: true },
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       if (result) {
         this.authService.logout();
-        this.router.navigate(['/login'])
-        localStorage.removeItem('rememberUsername')
-        localStorage.removeItem('rememberPassword')
+        this.router.navigate(['/login']);
+        localStorage.removeItem('rememberUsername');
+        localStorage.removeItem('rememberPassword');
       }
     });
+  }
+
+  applySearch() {
+    if (this.searchText) {
+      const input = this.searchText.toLowerCase();
+
+      this.filterData = this.data.filter((item) =>
+        item.name.toLowerCase().includes(input)
+      );
+
+      this.totalItems = this.filterData.length;
+
+      this.currentPage = 1;
+
+      this.isSearchActive = true;
+    } else {
+      this.filterData = this.data;
+
+      this.totalItems = this.data.length;
+
+      this.currentPage = 1;
+
+      this.isSearchActive = false;
+    }
   }
 
   onPageChange(pageNumber: number): void {
