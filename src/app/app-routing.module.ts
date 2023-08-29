3964a932aba46580411b6fa32750d707a0a8fe2a
quanglv99 <vanquang.vnl21@gmail.com>
2023-08-29 15:46:40 +0700

    lazy_loading_20230829

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,6 @@ const routes: Routes = [
   },
   {path:'forget',component:ForgetComponent},
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: '**', redirectTo: '/login' },
 ];
 
 @NgModule({
